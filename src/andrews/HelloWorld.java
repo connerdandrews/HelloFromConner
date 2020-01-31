742d3dde72f11ed780a02ed54640114fb18b8ac1
Connor Albright <alb19001@byui.edu>
2020-01-31 12:31:56 -0700

    Changed form connor.

--- src/andrews/HelloWorld.java
+++ src/andrews/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello, my name is Conner Andrews.");
+        System.out.println("Hello Conner, my name is Connor");
     }
 }
